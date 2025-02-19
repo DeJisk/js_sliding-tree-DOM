06cc70a70962c815847acaebcb5a97fe223c81d7
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-02-19 11:02:39 +0200

    make build

---
+++ index.c618f6a9.js
@@ -0,0 +1,2 @@
+document.querySelector(".tree").addEventListener("click",function(e){var n=e.target.nextElementSibling;n&&(n.style.display="none"===n.style.display?"block":"none")});
+//# sourceMappingURL=index.c618f6a9.js.map
